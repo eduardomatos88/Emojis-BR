2346b66bcd1157b112d6dfffadffc5921a610f6f
Eduardo Matos <luizmatosedu@gmail.com>
2020-09-24 18:02:08 -0300

    Adicionado limite de items na pesquisa Adicionada notificação de emoji copiado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactNotification, { store } from 'react-notifications-component'
 import './App.css';
+import 'react-notifications-component/dist/theme.css'
 
-import emojiList from './database/emojiList.json'
+import emojiListBase from './database/emojiList.json'
 
 function App() {
+  const [emojiList, setEmojiList] = useState(emojiListBase)
+
+
+  function searchEmoji(event) {
+    const searchTerm = event.target.value.toLowerCase()
+    if(searchTerm === '') {
+      setEmojiList(emojiListBase)
+    } else {
+      var contador = 0;
+      const searchResult = emojiListBase.filter(emoji => {
+        if(contador > 50) {
+          return false
+        }
+        if(emoji.traduzido.toLowerCase().indexOf(searchTerm) !== -1) {
+          contador++
+          return true
+        }else {
+          return false
+        }
+      })
+      contador = 0
+      setEmojiList(searchResult)
+    }
+  }
+  function copyEmojiToClipboard(emoji) {
+    const emojiReady = ':' + emoji + ':'
+    navigator.clipboard.writeText(emojiReady)
+    store.addNotification({
+      title: `Emoji "${emoji}" copiado`,
+      message: 'Agora é colar para utilizar o seu emoji!',
+      type: 'success',
+      container: 'top-right',
+      animationIn: ["animate__animated", "animate__fadeIn"],
+      animationOut: ["animate__animated", "animate__fadeOut"],
+      dismiss: {
+        duration: 5000,
+        onScreen: true
+      },
+    })
+  }
+
   return (
-    <div className="container">
-      {emojiList.map(emoji => (
-        <div className="emoji-container">
-          <img src={emoji.link} alt={emoji.emoji} className="emoji-image"></img>
-          <span className="emoji-traduzido">{emoji.traduzido}</span>
+    <>
+    <ReactNotification />
+      <div className="container">
+        <div className="title">
+          Git Emojis BR
         </div>
-      ))}
-    </div>
+        <div className="emoji-search">
+          <input onChange={searchEmoji} className="search" placeholder="Digite para buscar" />
+        </div>
+        <div className="emoji-list">
+          {emojiList.map(emoji => (
+            <div className="emoji-container" key={emoji.emoji} onClick={() => copyEmojiToClipboard(emoji.emoji)}>
+              <img src={emoji.link} alt={emoji.emoji} className="emoji-image"></img>
+              <span className="emoji-traduzido">{emoji.traduzido}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
