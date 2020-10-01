096eaea552d0c20a185d8c8f01b58ba3f793d3d1
Luiz Matos <luizmatos88@yahoo.com.br>
2020-10-01 14:32:16 -0300

    :genie: Layout da opágina repaginada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react'
 import ReactNotification, { store } from 'react-notifications-component'
-import './App.css';
+import './App.css'
 import 'react-notifications-component/dist/theme.css'
 
 import emojiListBase from './database/emojiList.json'
@@ -8,21 +8,20 @@ import emojiListBase from './database/emojiList.json'
 function App() {
   const [emojiList, setEmojiList] = useState(emojiListBase)
 
-
   function searchEmoji(event) {
     const searchTerm = event.target.value.toLowerCase()
-    if(searchTerm === '') {
+    if (searchTerm === '') {
       setEmojiList(emojiListBase)
     } else {
-      var contador = 0;
+      var contador = 0
       const searchResult = emojiListBase.filter(emoji => {
-        if(contador > 50) {
+        if (contador > 50) {
           return false
         }
-        if(emoji.traduzido.toLowerCase().indexOf(searchTerm) !== -1) {
+        if (emoji.traduzido.toLowerCase().indexOf(searchTerm) !== -1) {
           contador++
           return true
-        }else {
+        } else {
           return false
         }
       })
@@ -38,36 +37,48 @@ function App() {
       message: 'Agora é colar e utilizar o seu emoji!',
       type: 'success',
       container: 'top-right',
-      animationIn: ["animate__animated", "animate__fadeIn"],
-      animationOut: ["animate__animated", "animate__fadeOut"],
+      animationIn: ['animate__animated', 'animate__fadeIn'],
+      animationOut: ['animate__animated', 'animate__fadeOut'],
       dismiss: {
-        duration: 5000,
-        onScreen: true
+        duration: 3000,
+        onScreen: true,
       },
     })
   }
 
   return (
     <>
-    <ReactNotification />
+      <ReactNotification />
       <div className="container">
-        <div className="title">
-          Git Emojis BR
-        </div>
+        <h1 className="title">Git Emojis BR</h1>
+        <h2 className="subtitle">Seu buscador de emoji em português</h2>
         <div className="emoji-search">
-          <input onChange={searchEmoji} className="search" placeholder="Digite para buscar" />
+          <input
+            onChange={searchEmoji}
+            className="search"
+            placeholder="Digite para buscar"
+          />
         </div>
         <div className="emoji-list">
           {emojiList.map(emoji => (
-            <div className="emoji-container" key={emoji.emoji} onClick={() => copyEmojiToClipboard(emoji.emoji)}>
-              <img src={emoji.link} alt={emoji.emoji} className="emoji-image"></img>
-              <span className="emoji-traduzido">{emoji.traduzido}</span>
+            <div className="emoji-container" key={emoji.emoji}>
+              <img
+                src={emoji.link}
+                alt={emoji.emoji}
+                className="emoji-image"
+              ></img>
+              <span
+                className="emoji-traduzido"
+                onClick={() => copyEmojiToClipboard(emoji.emoji)}
+              >
+                {emoji.traduzido}
+              </span>
             </div>
           ))}
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
